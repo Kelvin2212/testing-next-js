bec87d36cb1b1ca64aa750ced95b506fb0e70f6b
Kelvin_Samuel <kelvinbalkes@gmail.com>
2024-03-13 00:00:34 +0700

    CHANGE ENV API URL

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -24,13 +24,46 @@ const DetailProductPage = ({ product }: { product: ProductType }) => {
 
 export default DetailProductPage;
 
-// export async function getServerSideProps({
+export async function getServerSideProps({
+  params,
+}: {
+  params: { product: string };
+}) {
+  console.log(params.product);
+  //fetch data
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/product/${params.product}`
+  );
+  const response = await res.json();
+
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
+
+// export async function getStaticPaths() {
+//   const res = await fetch("http://localhost:3000/api/product");
+//   const response = await res.json();
+
+//   const paths = response.data.map((product: ProductType) => ({
+//     params: {
+//       product: product.id,
+//     },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps({
 //   params,
 // }: {
 //   params: { product: string };
 // }) {
-//   console.log(params.product);
-//   //fetch data
 //   const res = await fetch(
 //     `http://localhost:3000/api/product/${params.product}`
 //   );
@@ -42,36 +75,3 @@ export default DetailProductPage;
 //     },
 //   };
 // }
-
-export async function getStaticPaths() {
-  const res = await fetch("http://localhost:3000/api/product");
-  const response = await res.json();
-
-  const paths = response.data.map((product: ProductType) => ({
-    params: {
-      product: product.id,
-    },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({
-  params,
-}: {
-  params: { product: string };
-}) {
-  const res = await fetch(
-    `http://localhost:3000/api/product/${params.product}`
-  );
-  const response = await res.json();
-
-  return {
-    props: {
-      product: response.data,
-    },
-  };
-}
